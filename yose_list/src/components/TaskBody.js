d375f7ab2dd2169e7a9e232f3eaff191e4e30cc7
tabspear <tabspear@github.com>
2022-07-19 14:54:35 -0700

    Feat(Client): Added TaskNav and AddIcon Component

--- yose_list/src/components/TaskBody.js
+++ yose_list/src/components/TaskBody.js
@@ -1,12 +1,15 @@
+import TaskNav from "./TaskNav"
 import TaskList from "./TaskList"
 import "./TaskBody.css"
 import QuickTask from "./QuickTask"
 import TaskDay from "./TaskDay"
+import AddIcon from "./AddIcon"
 const TaskBody = ()=>{
     return (<div className="Task_bg">
-        
+        <TaskNav/>
         <TaskDay/>
         <TaskList/>
+        <AddIcon/>
         <QuickTask/>
     </div>)
 }
